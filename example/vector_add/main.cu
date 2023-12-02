98835e5a32eb2f5ad075369b4ccf0bd0718c65b2
Guoliang He <gh512@cl.cam.ac.uk>
2023-12-02 20:24:34 +0000

    add cu vector add driver

--- example/vector_add/main.cu
+++ example/vector_add/main.cu
@@ -10,17 +10,6 @@
 #include <string.h>
 #include <string>
 
-
-// Must be multiples of 16 for wmma code to work
-#define MATRIX_M 16384
-#define MATRIX_N 16384
-#define MATRIX_K 16384
-
-// The only dimensions currently supported by WMMA
-const int WMMA_M = 16;
-const int WMMA_N = 16;
-const int WMMA_K = 16;
-
 // Define some error checking macros.
 #define cudaErrCheck(stat)                         \
     {                                              \
@@ -44,27 +33,6 @@ void cudaDriverCheck_(CUresult stat, const char* file, int line)
     }
 }
 
-#define curandErrCheck(stat)                         \
-    {                                                \
-        curandErrCheck_((stat), __FILE__, __LINE__); \
-    }
-
-void curandErrCheck_(curandStatus_t stat, const char* file, int line)
-{
-    if (stat != CURAND_STATUS_SUCCESS) {
-        fprintf(stderr, "cuRand Error: %d %s %d\n", stat, file, line);
-    }
-}
-
-
-__global__ void convertFp32ToFp16(half* out, float* in, int n)
-{
-    int idx = blockDim.x * blockIdx.x + threadIdx.x;
-    if (idx < n) {
-        out[idx] = in[idx];
-    }
-}
-
 char* concat(const char *s1, const char *s2)
 {
     char *result = (char*)malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
@@ -74,10 +42,7 @@ char* concat(const char *s1, const char *s2)
     return result;
 }
 
-void bench() {
-}
-
-void run(const char * fn, const char* func, int warmup, int repeats) {
+void run(const char * fn, const char* func, int n, int warmup, int repeats) {
     // load module
     char * file_name = concat(fn, ".cubin");
 	CUresult cuResult;
@@ -95,86 +60,49 @@ void run(const char * fn, const char* func, int warmup, int repeats) {
     cudaDriverCheck(cuModuleGetFunction(&kernel, module, func));
 
     // args
-    float* a_fp32;
-    float* b_fp32;
-    half* a_fp16;
-    half* b_fp16;
-
-    float* c;
-    float* c_wmma;
-
-    float* c_host_wmma;
-
-    curandGenerator_t gen;
+    size_t bytes = n * sizeof(float);
 
     cudaEvent_t startWMMA;
     cudaEvent_t stopWMMA;
-
     cudaErrCheck(cudaEventCreate(&startWMMA));
     cudaErrCheck(cudaEventCreate(&stopWMMA));
 
-    cudaErrCheck(cudaMalloc((void**)&a_fp32, MATRIX_M * MATRIX_K * sizeof(float)));
-    cudaErrCheck(cudaMalloc((void**)&b_fp32, MATRIX_K * MATRIX_N * sizeof(float)));
-    cudaErrCheck(cudaMalloc((void**)&a_fp16, MATRIX_M * MATRIX_K * sizeof(half)));
-    cudaErrCheck(cudaMalloc((void**)&b_fp16, MATRIX_K * MATRIX_N * sizeof(half)));
+    // Allocate memory on the host
+    float *h_a, *h_b, *h_c;
+    h_a = (float*)malloc(bytes);
+    h_b = (float*)malloc(bytes);
+    h_c = (float*)malloc(bytes);
 
-    cudaErrCheck(cudaMalloc((void**)&c, MATRIX_M * MATRIX_N * sizeof(float)));
-    cudaErrCheck(cudaMalloc((void**)&c_wmma, MATRIX_M * MATRIX_N * sizeof(float)));
+    // Initialize input vectors
+    for (int i = 0; i < n; i++) {
+        h_a[i] = i;
+        h_b[i] = 2 * i;
+    }
 
-    c_host_wmma = (float*)malloc(MATRIX_M * MATRIX_N * sizeof(float));
+    // Allocate memory on the device
+    float *d_a, *d_b, *d_c;
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
 
-    curandErrCheck(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT));
-    curandErrCheck(curandSetPseudoRandomGeneratorSeed(gen, 1337ULL));
+    // Copy vectors from host to device
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
 
-    curandErrCheck(curandGenerateUniform(gen, a_fp32, MATRIX_M * MATRIX_K));
-    curandErrCheck(curandGenerateUniform(gen, b_fp32, MATRIX_K * MATRIX_N));
+    printf("Vector add workload...\n");
+    printf("n: %d\n", n);
 
-    // curand doesn't currently support fp16 so we generate in fp32 and convert to fp16.
-    convertFp32ToFp16<<<(MATRIX_M * MATRIX_K + 255) / 256, 256>>>(a_fp16, a_fp32, MATRIX_M * MATRIX_K);
-    convertFp32ToFp16<<<(MATRIX_K * MATRIX_N + 255) / 256, 256>>>(b_fp16, b_fp32, MATRIX_K * MATRIX_N);
-
-    curandErrCheck(curandGenerateUniform(gen, c, MATRIX_M * MATRIX_N));
-
-    curandErrCheck(curandDestroyGenerator(gen));
-
-    cudaErrCheck(cudaMemcpy(c_wmma, c, MATRIX_M * MATRIX_N * sizeof(float), cudaMemcpyDeviceToDevice));
-
-	// int *output;
-	// cudaMalloc((void**)&output, sizeof(int)*128);
-
-
-	// void * args[1] = {&output};
-
-	// cuLaunchKernel(kernel, 1, 1, 1,
-	// 		32, 1, 1,
-	// 		32*1024, 0, args, 0);
-
-	// Set arguments
-	int M =  MATRIX_M;
-	int N =  MATRIX_N;
-	int K =  MATRIX_K;
-    float alpha = 2.0f;
-    float beta = 2.0f;
-
-    printf("MMA workload...\n");
-    printf("M = %d, N = %d, K = %d. alpha = %f, beta = %f\n\n", MATRIX_M, MATRIX_N, MATRIX_K, alpha, beta);
-
-	void* args[] = { &a_fp16, &b_fp16, &c_wmma, &M, &N, &K, &alpha, &beta };
+	void* args[] = { &d_a, &d_b, &d_c, &n};
 
     // kernel args
     dim3 gridDim;
     dim3 blockDim;
 
-    // blockDim.x must be a multple of warpSize
-    // 128x4 means we have 16 warps and a block computes a 64x64 output tile
-    blockDim.x = 128;
-    blockDim.y = 4;
+    blockDim.x = 256;
+    gridDim.x = (int)ceil((float)n / blockDim.x);
 
-    gridDim.x = (MATRIX_M + (WMMA_M * blockDim.x / 32 - 1)) / (WMMA_M * blockDim.x / 32);
-    gridDim.y = (MATRIX_N + WMMA_N * blockDim.y - 1) / (WMMA_N * blockDim.y);
-
-    printf("gridDim %d, %d\n", gridDim.x, gridDim.y);  // 256, 256
-    printf("blockDim %d, %d\n", blockDim.x, blockDim.y);  // 128, 4
+    printf("gridDim %d, %d\n", gridDim.x, gridDim.y);
+    printf("blockDim %d, %d\n", blockDim.x, blockDim.y);
 
     // Launch the kernel
     cuLaunchKernel(
@@ -234,15 +162,24 @@ void run(const char * fn, const char* func, int warmup, int repeats) {
 
 int main(int argc, char* argv[]) {
     std::string filename = "out"; // Default filename
+    int n = 1000000;  // default 1M
     
     // Parse command line arguments
     for (int i = 1; i < argc; ++i) {
         if (std::string(argv[i]) == "--fn") {
             if (i + 1 < argc) {
                 filename = argv[i + 1]; // Get the filename argument
-                break;
+                i++; // Skip the filename argument
             } else {
                 std::cerr << "Error: --fn requires an argument." << std::endl;
+                return 1;
+            }
+        } else if (std::string(argv[i]) == "--n") {
+            if (i + 1 < argc) {
+                n = std::atoi(argv[i + 1]); // Get the value for n
+                i++; // Skip the n argument
+            } else {
+                std::cerr << "Error: --n requires an argument." << std::endl;
                 return 1;
             }
         }
@@ -254,5 +191,5 @@ int main(int argc, char* argv[]) {
     // Convert filename to char*
     const char* filenameChar = filename.c_str();
 
- 	run(filenameChar, "_Z12wmma_exampleP6__halfS0_Pfiiiff", 5, 20);
+ 	run(filenameChar, "_Z9vectorAddPfS_S_i", n, 5, 50);
 }
